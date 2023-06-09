901c85442b64d06064cb8292bab2ef5aadb92eb0
guga56 <gustavogregorio086@gmail.com>
2023-06-09 16:12:51 -0300

    criando os repositorio

---
+++ Desenvolvimento.Dominio/Repository/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desenvolvimento.Dominio.Repository
+{
+    public class BaseRepository
+    {
+
+    }
+}
